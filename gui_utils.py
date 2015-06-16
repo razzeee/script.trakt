8422f66b6a6afd6b132ffd7868a149871db1259a
Kolja Lampe <razzeee@gmail.com>
2015-06-16 17:26:31 +0200

    Moved edit control from code to xml

--- gui_utils.py
+++ gui_utils.py
@@ -15,8 +15,8 @@ def get_pin():
     ACTION_PREVIOUS_MENU = 10
     ACTION_BACK = 92
     INSTRUCTION_LABEL = 203
-    CENTER_Y = 6
-    CENTER_X = 2
+
+    PIN_EDIT = 3001
 
     logger = logging.getLogger(__name__)
     
@@ -24,20 +24,20 @@ def get_pin():
         auth = False
         
         def onInit(self):
-            self.pin_edit_control = self.__add_editcontrol(30, 240, 40, 450)
-            self.setFocus(self.pin_edit_control)
+            self.pin = self.getControl(PIN_EDIT)
+            self.setFocus(self.pin)
             auth = self.getControl(AUTH_BUTTON)
             never = self.getControl(NEVER_BUTTON)
             instuction = self.getControl(INSTRUCTION_LABEL)
-            instuction.setLabel( "1) " + getString(32159).format("[COLOR red]http://trakt.tv/pin/999[/COLOR]") + "\n2) " + getString(32160) + "\n3) " + getString(32161) + "\n\n" + getString(32162))
-            self.pin_edit_control.controlUp(never)
-            self.pin_edit_control.controlLeft(never)
-            self.pin_edit_control.controlDown(auth)
-            self.pin_edit_control.controlRight(auth)
-            auth.controlUp(self.pin_edit_control)
-            auth.controlLeft(self.pin_edit_control)
-            never.controlDown(self.pin_edit_control)
-            never.controlRight(self.pin_edit_control)
+            instuction.setLabel("1) " + getString(32159).format("[COLOR red]http://trakt.tv/pin/999[/COLOR]") + "\n2) " + getString(32160) + "\n3) " + getString(32161) + "\n\n" + getString(32162))
+            self.pin.controlUp(never)
+            self.pin.controlLeft(never)
+            self.pin.controlDown(auth)
+            self.pin.controlRight(auth)
+            auth.controlUp(self.pin)
+            auth.controlLeft(self.pin)
+            never.controlDown(self.pin)
+            never.controlRight(self.pin)
             
         def onAction(self, action):
             #print 'Action: %s' % (action.getId())
@@ -74,7 +74,7 @@ def get_pin():
                 self.close()
         
         def __get_token(self):
-            pin = self.pin_edit_control.getText().strip()
+            pin = self.pin.getText().strip()
             if pin:
                 try:
                     if traktapi.traktAPI().authenticate(pin):
@@ -82,18 +82,7 @@ def get_pin():
                 except:
                     return False
             return False
-        
-        # have to add edit controls programatically because getControl() (hard) crashes XBMC on them
-        def __add_editcontrol(self, x, y, height, width):
-            media_path = os.path.join(__addon__.getAddonInfo('path'), 'resources', 'skins', 'Default', 'media')
-            temp = xbmcgui.ControlEdit(0, 0, 0, 0, '', font='font12', textColor='0xFFFFFFFF', focusTexture=os.path.join(media_path, 'button-focus2.png'),
-                                       noFocusTexture=os.path.join(media_path, 'button-nofocus.png'), _alignment=CENTER_Y | CENTER_X)
-            temp.setPosition(x, y)
-            temp.setHeight(height)
-            temp.setWidth(width)
-            self.addControl(temp)
-            return temp
-        
+
     dialog = PinAuthDialog('script-trakt-PinAuthDialog.xml', __addon__.getAddonInfo('path'))
     dialog.doModal()
     if dialog.auth:
