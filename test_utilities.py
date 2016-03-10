01b141a6337b642078e38e4ab5443061e9a87f6f
Razzeee <razzeee@gmail.com>
2016-03-10 16:32:54 +0100

    Add simple tests and add .travis ci

--- test_utilities.py
+++ test_utilities.py
@@ -1,10 +1,7 @@
-import unittest
+import pytest
 import utilities
 
-class TestUtilities(unittest.TestCase):
-
-    def setUp(self):
-        pass
+class TestUtilities:
 
     def test_isMovie(self):
         assert utilities.isMovie('movie')
@@ -20,3 +17,9 @@ class TestUtilities(unittest.TestCase):
 
     def test_parseIdToTraktIds_IMDB(self):
         assert utilities.parseIdToTraktIds('tt1431045', 'movie')[0] == {'imdb': 'tt1431045'}
+
+    def test_parseIdToTraktIds_TMDB(self):
+        assert utilities.parseIdToTraktIds('20077', 'movie')[0] == {'tmdb': '20077'}
+
+    def test_parseIdToTraktIds_Tvdb(self):
+        assert utilities.parseIdToTraktIds('4346770', 'show')[0] == {'tvdb': '4346770'}
